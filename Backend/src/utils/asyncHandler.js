35d60e8963cea73305ef12a56a229da8cbcae046
Shivam <shivamch0865@gmail.com>
2026-02-05 20:10:16 +0530

    Created Utils

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+// Promises // 
+const asyncHandler = (requestHandler) => {
+    return (req , res , next) => {
+        Promise.resolve(requestHandler(req , res , next))
+        .catch((error) => next(error))
+    }
+}
+
+// Try Catch // 
+// const asyncHandler = (fn) => async ( req , res , next) => {
+//     try {
+//         await fn(req , res , next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success : false,
+//             message : err.message
+//         })
+//     }
+// }
+
+export {asyncHandler};

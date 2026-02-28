920568bc7a5cc44d3cb3a4923d5907572ae8d098
Shivam <shivamch0865@gmail.com>
2026-02-28 21:45:35 +0530

    app.js correct

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path"
 
 const app = express();
 
@@ -30,14 +29,5 @@ app.use("/api/v1/chat" , chatRoute);
 app.use("/api/v1/emotion" , emotionRoute);
 app.use("/api/v1/profile", profileRoute);
 
-const __dirname = path.resolve();
-
-// Serve frontend build
-app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-// SPA fallback
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
 
 export default app;
